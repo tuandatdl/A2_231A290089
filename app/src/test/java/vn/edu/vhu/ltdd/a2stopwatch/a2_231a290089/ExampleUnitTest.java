aa295f1600aa53b8aa6108addf2308ce85f1854a
tuandatdl <doletuandat@gmail.com>
2026-09-20 12:55:35 +0700

    A2: Xu ly onSaveInstanceState va hoan thien kien truc

---
+++ app/src/test/java/vn/edu/vhu/ltdd/a2stopwatch/a2_231a290089/ExampleUnitTest.java
@@ -0,0 +1,17 @@
+package vn.edu.vhu.ltdd.a2stopwatch.a2_231a290089;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
+}
